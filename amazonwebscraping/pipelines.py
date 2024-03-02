6c0cdc524cae99669ab44a0c0e12952454568861
SDHAKAL07 <dhakalsuman07@gmail.com>
2024-03-02 23:35:12 +0545

    COM-02: Database connection and user agent config change

--- amazonwebscraping/pipelines.py
+++ amazonwebscraping/pipelines.py
@@ -6,13 +6,36 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import sqlite3
 
 class AmazonwebscrapingPipeline:
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+        pass
     def process_item(self, item, spider):
-        print("Pipeline :" + item['name'][0])
-        print("Pipeline :" + item['price'])
-        print("Pipeline :" + item['review'])
-        print("Pipeline :" + item['availability'][0])
-        print("Pipeline :" + item['image_link'])
+        self.hydrate_table(item)
         return item
+    def create_connection(self):
+        self.con = sqlite3.connect('amazon.db')
+        self.curr = self.con.cursor()
+        
+    def hydrate_table(self, item):
+        self.curr.execute(""" INSERT INTO products values (?,?,?,?,?)""",(
+        item['name'],
+        item['price'],
+        item['review'],
+        item['availability'],
+        item['image_link']
+        ))
+        self.con.commit()
+
+    def create_table(self):
+        self.curr.execute("""
+            create table if not exists products(
+            product_name text,
+            product_price text,
+            product_review text,
+            product_type text,
+            product_imagelink text
+            )""")
